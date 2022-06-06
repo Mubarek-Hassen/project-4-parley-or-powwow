e959bb7068c207dddab5785ca9bfdc368046fe10
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-06-06 11:55:23 -0400

    mock DB added, displayed.

--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,24 @@ class Home(TemplateView):
 
 class About(TemplateView):
     template_name = "about.html"
+
+class Post:
+    def __init__(self, author, title, body, img):
+        self.author = author
+        self.title = title
+        self.body = body
+        self.img = img
+
+letters = [
+    Post('Unknown Author', 'The Best Quote', 'Write A Wise Saying And Your Name Will Live Forever!', 'https://thomasguettler.files.wordpress.com/2021/02/wiseman.jpg'),
+    Post('Cat', 'Purr', 'Meow Meow, Meow Meow Mew Meow, MEOW!! HISS! The End!', 'https://ih1.redbubble.net/image.3087347788.1442/st,small,507x507-pad,600x600,f8f8f8.jpg')
+]
+
+
+class Posts(TemplateView):
+    template_name = "posts.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['letters'] = letters
+        return context
