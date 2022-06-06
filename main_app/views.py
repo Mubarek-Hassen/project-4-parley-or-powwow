5457bb8aac53e7dec4a1e41d47b1a9c4966e45f8
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-06-06 10:01:20 -0400

    Testing templates added

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 # Create your views here.
 
-class Home(View):
-    def get(self, request):
-        return HttpResponse('The Parley')
+class Home(TemplateView):
+    template_name = "home.html"
 
-class About(View):
-    def get(self, request):
-        return HttpResponse('The Parley About')
+class About(TemplateView):
+    template_name = "about.html"
