818943535ff22f872015fe6a5dfb0e009509e3b6
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-06-07 15:36:24 -0400

    User added

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,6 @@
 # from unicodedata import name
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -11,5 +11,8 @@ class Blog(models.Model):
     img = models.CharField(max_length=500)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
     class Meta:
         ordering = ['-date']
